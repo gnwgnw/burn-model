eac4c7c94304ebf43676401293226a4353681c7a
Serg Titaevskiy <Titaevskiy.S@gmail.com>
2015-12-21 14:27:45 +0300

    Extract function

--- calculate_u.m
+++ calculate_u.m
@@ -1,55 +1,53 @@
-function main
-    right = 0.01;
-    burn_edge = 1e-3;
+function [u_t, g, dg] = calculate_u(lambda,R,Cp,Pk,Ts,Tmax,rho_t,Ak,Ea,Gt,Cpt,Ts0,right,burn_edge,right_out,N)
+%   right - right end of the boundary
+%   burn_edge - boundary of the g changes
+%   right_out - right end of the x for g
+%   N - count of the point in the mesh
+%
+%   right >= right_out >= burn_edge
+
+%     right = 0.01;
+%     burn_edge = 1e-3;
+%     right_plot = 1e-3; 
+%     N = 2000;
+%
+%     Gt = -5e4;
+%     Cpt = 1464;
+%     Ts0 = 300;
+%
+%     lambda = 0.23;
+%     R = 363;
+%     Cp = 1800;
+%     Pk = 1e7;
+%     Ts = 720;
+%     Tmax = 2360;
+%     rho_t = 1600;
+%     Ak = 1e8;
+%     Ea = 4e6;
+
     burn_mesh_dens = 0.9;
-    
-    right_plot = 1e-3;
-    
-    N = 2000;
     
     x_mesh = unique([
         linspace(0, burn_edge, burn_mesh_dens*N) linspace(burn_edge, right, (1-burn_mesh_dens)*N)
     ]);
-    
-    Gt = -5e4;
-    Cpt = 1464;
-    Ts0 = 300;
-    
-    lambda = 0.23;
-    R = 363;
-    Cp = 1800;
-    Pk = 1e7;
-    Ts = 720;
-    Tmax = 2360;
-    rho_t = 1600;
-    Ak = 1e8;
-    Ea = 4e6;
 
     [A,B,C,D,E,F] = params(lambda,R,Cp,Pk,Ts,Tmax,rho_t,Ak,Ea,Gt,Cpt,Ts0);
     
     solinit = bvpinit(x_mesh, [1 1]);
     sol = bvp5c(@(x,y) ode(x,y,A,B,C,D,E,F), @bc, solinit);
     
-    x = linspace(0, right_plot, N);
+    x = linspace(0, right_out, N);
+    
     g = deval(sol, x);
-   
-    figure(1);
-    plot(x, g(1,:));
-    
-    figure(2);
     dg = g(2,:);
-    plot(x, dg);
-    
+    g = g(1,:);
+  
     dgdx_0 = dg(1);
-%     dgdx_0 = max(dg);
-    
-    u_t_new = -lambda*dgdx_0*(Tmax - Ts)/(rho_t*(Gt+Cpt*(Ts-Ts0)));
-%     u_t_D = sol.parameters * Pk / (rho_t * R);
-    
+    u_t = -lambda*dgdx_0*(Tmax - Ts)/(rho_t*(Gt+Cpt*(Ts-Ts0)));
+
+    fprintf('Pk = %e\n', Pk);
     fprintf('dgdx_0 = %7.6f\n', dgdx_0);
-    fprintf('u_t_new = %e\n', u_t_new);
-%     fprintf('D = %e\n', sol.parameters);
-%     fprintf('u_t_D = %e\n', u_t_D);
+    fprintf('u_t_new = %e\n', u_t);
 
 end
 
