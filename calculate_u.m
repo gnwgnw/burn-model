2759561929b1a0bab0d885554609257f9ea00bf4
Serg Titaevskiy <Titaevskiy.S@gmail.com>
2015-12-23 15:37:18 +0300

    Fix bug: C1 param without Cp

--- calculate_u.m
+++ calculate_u.m
@@ -68,7 +68,7 @@ function res = bc(ya,yb)
 end
 
 function [C1,C2,B,C,F] = params(lambda,R,Cp,Pk,Ts,Tmax,Ak,Ea,Gt,Cpt,Ts0)
-    C1 = (Ts - Tmax)/(Gt+Cpt*(Ts - Ts0));
+    C1 = Cp*(Ts - Tmax)/(Gt+Cpt*(Ts - Ts0));
     C2 = Ak*Cp*Pk/(lambda*R);
     B = Ts;
     C = Tmax - Ts;
